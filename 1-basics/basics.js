1b99c19e5147a622d4e917f644f051a10254db01
sarahoeri <oerisarah@gmail.com>
2019-10-26 17:52:19 +0300

    add2

--- 1-basics/basics.js
+++ 1-basics/basics.js
@@ -76,7 +76,9 @@ describe("basics", function() {
             // new Number() creates a "primitive wrapper". It's not an actual number, it's an object.
             // In 99.999% of all cases, this is a bug and not actually intended. Fix it! :)
             // ✏️ FIX THAT BUG ✏️
-            const someNumber = new Number(3);
+            const someNumber = 3;
+            someNumber == 3;
+            someNumber === 3;
             ////////////////////////////////////////////////////////////////////
 
             // The primitive wrapper passes the loose quality check because it will be casted to a number.
