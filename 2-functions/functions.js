104f75ad6ed353941233903f7aec8f5d91db8628
sarahoeri <oerisarah@gmail.com>
2019-11-28 21:29:33 +0300

    with explicit content

--- 2-functions/functions.js
+++ 2-functions/functions.js
@@ -98,7 +98,9 @@ describe("functions", function() {
             // and 'answerThirdResult' with the correct results.
             // If you call getContext() here, you're a cheater! :)
             // ✏️ ADD CODE HERE ✏️
-
+            const answerFirstResult = undefined;
+            const answerSecondResult = context;
+            const answerThirdResult = undefined;
             ////////////////////////////////////////////////////////////////////
 
             // First we call the function without an explicit context
