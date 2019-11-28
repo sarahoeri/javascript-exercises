dd8d3f093a7801cce213084d940134ffeb8659cd
sarahoeri <oerisarah@gmail.com>
2019-11-28 21:25:55 +0300

    explicit content

--- 2-functions/functions.js
+++ 2-functions/functions.js
@@ -74,7 +74,7 @@ describe("functions", function() {
             // Declare a constant 'answer' with the correct result.
             // If you call getContext() here, you're a cheater! :)
             // ✏️ ADD CODE HERE ✏️
-
+            const answer = undefined;
             ////////////////////////////////////////////////////////////////////
 
             const result = getContext(); // <--- called without explicit context
