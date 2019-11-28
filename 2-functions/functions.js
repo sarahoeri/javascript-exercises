a76b11988c9f7550408e08db9af87649b1860221
sarahoeri <oerisarah@gmail.com>
2019-11-28 22:18:49 +0300

    reference itself

--- 2-functions/functions.js
+++ 2-functions/functions.js
@@ -337,7 +337,9 @@ describe("functions", function() {
             ////////////////////////////////////////////////////////////////////
             // Can you implement a function 'myFunction' that returns a reference to itself?
             // ✏️ ADD CODE HERE ✏️
-
+            function myFunction() {
+                return myFunction;
+            }
             ////////////////////////////////////////////////////////////////////
 
             const result = myFunction();
