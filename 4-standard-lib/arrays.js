66421ecbdb5bf2176cc910dc077e823a88680b33
sarahoeri <oerisarah@gmail.com>
2019-11-29 20:53:35 +0300

    sort

--- 4-standard-lib/arrays.js
+++ 4-standard-lib/arrays.js
@@ -190,7 +190,9 @@ describe("arrays", function() {
 
             ////////////////////////////////////////////////////////////////////
             // ✏️ ADD CODE HERE. USE THE STANDARD LIB, NO LOOPS! ✏️
-
+            function sortAlphabetically() {
+                return array.sort();
+            }
             ////////////////////////////////////////////////////////////////////
 
             expect(sortByLength()).to.equal(array);
