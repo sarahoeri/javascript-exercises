3e14cfacd64085fc2c88d1609754d8202d0aa14e
sarahoeri <oerisarah@gmail.com>
2019-11-29 22:28:37 +0300

    capital letter

--- 4-standard-lib/strings.js
+++ 4-standard-lib/strings.js
@@ -152,7 +152,9 @@ describe("strings", function() {
 
             ////////////////////////////////////////////////////////////////////
             // ✏️ ADD CODE HERE. USE THE STANDARD LIB! ✏️
-
+            function transformToCapitalLetters(string){
+                return text.toUpperCase(string);
+            }
             ////////////////////////////////////////////////////////////////////
 
             expect(transformToCapitalLetters()).to.equal(
