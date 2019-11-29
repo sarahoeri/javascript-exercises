944eb5d223ad377742db41940a2ae0d1b323def3
sarahoeri <oerisarah@gmail.com>
2019-11-29 19:58:32 +0300

    remove last item

--- 4-standard-lib/arrays.js
+++ 4-standard-lib/arrays.js
@@ -12,7 +12,9 @@ describe("arrays", function() {
         it("should run without errors", function() {
             ////////////////////////////////////////////////////////////////////
             // ✏️ ADD CODE HERE. USE THE STANDARD LIB, NO LOOPS! ✏️
-
+            function getNumberOfItems(array){
+                return array.length;
+            }
             ////////////////////////////////////////////////////////////////////
 
             expect(getNumberOfItems([])).to.equal(0);
@@ -28,7 +30,9 @@ describe("arrays", function() {
 
             ////////////////////////////////////////////////////////////////////
             // ✏️ ADD CODE HERE. USE THE STANDARD LIB, NO LOOPS! ✏️
-
+            function removeLastItem(){
+                return array.pop();
+            }
             ////////////////////////////////////////////////////////////////////
 
             expect(removeLastItem()).to.equal("d");
