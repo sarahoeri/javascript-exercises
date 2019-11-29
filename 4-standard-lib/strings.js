cefe71edbccf4cc1e094f63e502d1d247b2f055d
sarahoeri <oerisarah@gmail.com>
2019-11-29 22:00:56 +0300

    string partial

--- 4-standard-lib/strings.js
+++ 4-standard-lib/strings.js
@@ -14,7 +14,9 @@ describe("strings", function() {
 
             ////////////////////////////////////////////////////////////////////
             // ✏️ ADD CODE HERE. USE THE STANDARD LIB! ✏️
-
+            function findIndexOf(string){
+                return text.indexOf(string);
+            }
             ////////////////////////////////////////////////////////////////////
 
             expect(findIndexOf("fox")).to.equal(16);
