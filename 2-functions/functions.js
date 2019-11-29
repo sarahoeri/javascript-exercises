ae38b6dcc0ca87103867423fe8b27499925a1b23
sarahoeri <oerisarah@gmail.com>
2019-11-29 19:40:42 +0300

    lost content

--- 2-functions/functions.js
+++ 2-functions/functions.js
@@ -633,7 +633,7 @@ describe("functions", function() {
 
             // Now we're registering the service.handleEvent method so that
             // it will be called as soon as the event is emitted.
-            eventEmitter.addEventListener(service.handleEvent);
+            eventEmitter.addEventListener(service.handleEvent.bind(service));
             // Now let's emit the event. However, this will fail.
             // Find the reason why and fix it.
             eventEmitter.emit();
